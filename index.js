0f3ea864e44236833761123c851c5e897fe53796
ChisomSylvia <chisomsylvia95@gmail.com>
2024-05-01 12:15:06 +0100

    add more functionality and design

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
 
-const task = document.getElementById("task");
+const inputBox = document.getElementById("inputBox");
 const dueDate = document.getElementById("date");
-const addTask = document.getElementById("btn");
-const toDos = document.getElementById("outputDiv");
+const taskList = document.getElementById("taskList");
 
+//Add Task Function
+function addTask() {
+    if (inputBox.value === '' || dueDate.value === '') {
+    //if (inputBox.value === "") { 
+        alert("Enter Your To Dos!");
+    } 
+    else {
+        let list = document.createElement("li");
+        list.innerHTML = `${dueDate.value} ${inputBox.value}`;
+        taskList.appendChild(list);
 
-addTask.addEventListener("click", function () {
-    let para = document.createElement("p")
-    para.innerText = `${dueDate.value}, ${task.value}`;
-    toDos.appendChild(para);
-    task.value = "";
+        let del = document.createElement("span");
+        del.innerHTML = "\u00d7"
+        list.appendChild(del);
+    }
+
+    inputBox.value = "";
     dueDate.value = "";
+
+    saveTask()
+}
+
+// click function
+taskList.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveTask()
+    } 
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveTask()
+    }
+    else {
+        false;
+    }
 })
+
+// To save already created list
+function saveTask() {
+    localStorage.setItem("task", taskList.innerHTML);
+}
+
+function showTask() {
+    taskList.innerHTML = localStorage.getItem("task");
+}
+showTask()
